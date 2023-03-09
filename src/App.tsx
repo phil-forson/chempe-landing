2997d49e6b520d850fd1918c476d276382efcf4a
Philemon Forson <firstkoolaid21@gmail.com>
2023-03-09 08:15:09 +0000

    complete desktop design

--- src/App.tsx
+++ src/App.tsx
@@ -8,57 +8,24 @@ import Loyalty from "../src/assets/loyalty.png";
 import GoogleReviews from "../src/assets/googlerev.png";
 import GooglePlus from "../src/assets/google+.png";
 import LoyaltyIcon from "../src/assets/loyaltyicon.png";
-import Lagrilla from "../src/assets/lagrilla.png"
-import Azemera from "../src/assets/azemera.png"
-import Capitol from "../src/assets/capitol.png"
-import Marriot from "../src/assets/marriot.png"
+import Lagrilla from "../src/assets/lagrilla.png";
+import Lagrilla2 from "../src/assets/la-grilla.png";
+import Azemera from "../src/assets/azemera.png";
+import Azemera2 from "../src/assets/aze-mera.png";
+import Capitol from "../src/assets/capitol.png";
+import Capitol2 from "../src/assets/capi-tol.png";
+import Marriot from "../src/assets/marriot.png";
+import Ceo from "../src/assets/CEO.png";
+import Visa from "../src/assets/visa.png";
+import GooglePay from "../src/assets/google-pay.png";
+import ApplePay from "../src/assets/apple-pay.png";
+import MTN from "../src/assets/mtn.png";
+import Vodafone from "../src/assets/vodaphone.png";
+import MasterCard from "../src/assets/Mastercard.png";
 
 function App() {
-  // const addCatToBackend = () => {
-  //   axios.get('https://fakestoreapi.com/products/categories').then((res) => {
-  //    if(res.status === 200){
-  //     console.log(res.data)
-  //     const reqBody = res.data.map((cat: any) => {
-  //       return {
-  //         name: cat,
-  //         category: ''
-  //       }
-  //     })
-  //     reqBody.map((req: any) => {
-  //       axios.post('http://localhost:3000/categories', req).then((res) => {
-  //         if(res.status === 201){
-  //           console.log(res)
-  //         }
-  //       })
-  //     })
-  //    }
-  //   })
-  // }
-
-  // const addProductsToBackend = () => {
-  //   axios.get('https://fakestoreapi.com/products/').then((res1) => {
-  //     if(res1.status === 200){
-  //       axios.get('http://localhost:3000/categories').then((res2) => {
-  //         const reqBody = res1.data.map((item: any) => {
-  //           return {
-  //             name: item.title,
-  //             price: item.price,
-  //             description: item.description,
-  //             category: res2.data.find((res: any) => res.name === item.category)?.id
-  //           }
-  //         })
-  //         axios.post('http://localhost:3000/products', reqBody).then((res3) => {
-  //           if(res3.data === 200){
-  //             console.log(res3.data)
-  //           }
-  //         })
-  //         console.log(reqBody)
-  //       })
-  //           }
-  //   })
-  // }
   return (
-    <div className="h-full w-full vsm:px-0  vsm:pt-5 lg:py-10">
+    <div className="h-full w-full vsm:px-0  vsm:pt-5 lg:pt-10">
       <nav className="lg:px-[93px] vsm:px-5 vsm:pb-3  lg:pb-8 flex justify-between items-center w-full">
         <div className="">
           <img src={Logo} width={150} />
@@ -221,7 +188,9 @@ function App() {
         </div>
       </section>
       <section className="h-auto bg-primary px-[200px] py-[90px]">
-        <div className="text-[30px] w-full font-bold text-white text-center">More than 30 restaurants trust us</div>
+        <div className="text-[30px] w-full font-bold text-white text-center">
+          More than 30 restaurants trust us
+        </div>
         <div className="flex gap-x-6 justify-center items-center mt-[90px]">
           <img src={Capitol} />
           <img src={Lagrilla} />
@@ -229,7 +198,205 @@ function App() {
           <img src={Marriot} />
         </div>
       </section>
-      
+      <section className="px-[200px] py-[90px] h-auto">
+        <div className="flex flex-wrap h-[100vh] gap-x-8">
+          <div className="mt-10 ">
+            <div className="text-primary testimonial pl-[20px] relative">
+              Testimonials
+            </div>
+            <div className="text-primary font-bold text-[25px]">
+              Hear what our clients say
+            </div>
+            <div className="mt-10">
+              <div className="card px-10 py-10 flex flex-col w-[500px] h-[350px]">
+                <div className="">
+                  <img src={Lagrilla2} className="w-[150px] h-[86px]" />
+                </div>
+                <div className="mt-4">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Ducimus repellat cum vel, accusantium beatae tempore numquam!
+                  Officiis porro saepe assumenda!
+                </div>
+                <div className="flex mt-4 gap-x-[20px] items-center">
+                  <div className="rounded-full custom-bg h-full">
+                    <img src={Ceo} className="h-[40px] align-middle" />
+                  </div>
+                  <div className="flex flex-col justify-center ">
+                    <div className="font-bold">CEO</div>
+                    <div className="">La Grilla Restaurant</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-y-6">
+            <div className="card px-10 py-10 flex flex-col w-[500px] h-[350px]">
+              <div className="">
+                <img src={Azemera2} className="w-fit h-[86px]" />
+              </div>
+              <div className="mt-4">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Ducimus repellat cum vel, accusantium beatae tempore numquam!
+                Officiis porro saepe assumenda!
+              </div>
+              <div className="flex mt-4 gap-x-[20px] items-center">
+                <div className="rounded-full custom-bg h-full">
+                  <img src={Ceo} className="h-[40px] align-middle" />
+                </div>
+                <div className="flex flex-col justify-center ">
+                  <div className="font-bold">Managing Director</div>
+                  <div className="">Azmera Restaurant</div>
+                </div>
+              </div>
+            </div>
+            <div className="card px-10 py-10 flex flex-col w-[500px] h-[350px]">
+              <div className="">
+                <img src={Capitol2} className="w-fit h-fit" />
+              </div>
+              <div className="mt-4">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Ducimus repellat cum vel, accusantium beatae tempore numquam!
+                Officiis porro saepe assumenda!
+              </div>
+              <div className="flex mt-4 gap-x-[20px] items-center">
+                <div className="rounded-full custom-bg h-full">
+                  <img src={Ceo} className="h-[40px] align-middle" />
+                </div>
+                <div className="flex flex-col justify-center ">
+                  <div className="font-bold">Chief Operating Officer</div>
+                  <div className="">Capitol Restaurant</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="px-10 py-10">
+        <div className="w-full text-center text-primary font-bold text-[30px]">
+          Accept more payment methods
+        </div>
+        <div className="flex gap-x-6 justify-center items-center mt-[40px]">
+          <img src={GooglePay} />
+          <img src={ApplePay} />
+          <img src={MTN} />
+          <img src={Vodafone} />
+          <img src={Visa} />
+          <img src={MasterCard} />
+        </div>
+      </section>
+      <section className="px-10 py-[90px]">
+        <div className="custom-bg px-[120px] flex gap-x-[150px] py-[50px] rounded-[20px]">
+          <div className="flex flex-col text-primary w-[400px]">
+            <div className="font-bold text-[30px]">Free Demo</div>
+            <div className="">
+              Fill out the form so that one of our specialists will call you and
+              show you how Chem-pe can help you optimize operations in your
+              restaurant and sell more with our solutions.
+            </div>
+          </div>
+          <div className="flex gap-x-[70px]">
+            <div className="flex flex-col">
+              <label htmlFor="fullname" className="mb-2 font-bold">
+                Full name <span className="text-red-700">*</span>
+              </label>
+              <input
+                name="fullname"
+                className="h-[47px] px-3 outline-none mb-5 custom-input"
+              />
+              <label htmlFor="number" className="mb-2 font-bold">
+                Mobile Number <span className="text-red-700">*</span>
+              </label>
+              <input
+                name="number"
+                className="h-[47px] px-3 outline-none mb-5 custom-input"
+              />
+              <label htmlFor="pos" className="mb-2 font-bold">
+                POS System <span className="text-red-700">*</span>
+              </label>
+              <input
+                name="pos"
+                className="h-[47px] px-3 outline-none custom-input"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="email" className="mb-2 font-bold">
+                Email <span className="text-red-700">*</span>
+              </label>
+              <input
+                name="email"
+                className="h-[47px] px-3 outline-none mb-5 custom-input"
+              />
+              <label htmlFor="restaurant" className="mb-2 font-bold h-6">
+                Restaurant Name <span className="text-red-700">*</span>
+              </label>
+              <input
+                name="restaurant"
+                className="h-[47px] px-3 outline-none mb-5 custom-input"
+              />
+              <div className="h-6 mb-2"></div>
+              <button className="outline-none border-none bg-primary w-full rounded-[8px] h-[47px] text-white">
+                Request Demo
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="mt-[50px] py-[80px] px-[250px] bg-primary text-white h-auto flex gap-x-[50px] w-full items-center justify-between">
+        <div className="">
+          <div className="font-bold text-[15px]">Social Media</div>
+          <div className="flex flex-col">
+            <div className="">
+              <a href="">Instagram</a>
+            </div>
+            <div className="">
+              <a href="">LinkedIn</a>
+            </div>
+            <div className="">
+              <a href="">Facebook</a>
+            </div>
+          </div>
+        </div>
+        <div className="">
+          <div className="font-bold text-[15px]">About Chempe</div>
+          <div className="flex flex-col">
+            <div className="">
+              <a href="">Our History</a>
+            </div>
+            <div className="">
+              <a href="">Discover our blog</a>
+            </div>
+            <div className="">
+              <a href="">Appointment with us</a>
+            </div>
+          </div>
+        </div>
+        <div className="">
+          <div className="font-bold text-[15px]">Legal</div>
+          <div className="flex flex-col">
+            <div className="">
+              <a href="">Terms And Conditions</a>
+            </div>
+            <div className="">
+              <a href="">Privacy Policy</a>
+            </div>
+            <div className="">
+              <a href="">Cookies</a>
+            </div>
+          </div>
+        </div>
+        <div className="">
+          <div className="font-bold text-[15px] mb-2">Join our newsletter</div>
+          <div className="flex flex-col">
+            <input
+              name="restaurant"
+              className="h-[37px] px-3 outline-none mb-2 text-black"
+            />
+            <button className="outline-none border-none orange-linear w-full h-[37px] text-white">
+              Request Demo
+            </button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
